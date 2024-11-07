6cfe9717f2f8ea3b811c23846b3ede704e180c41
Raphael Manayon <133437120+rephy@users.noreply.github.com>
2024-11-07 07:43:20 -0800

    Video 2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+#include <iomanip>
 
-consteval int get_version() {
-    return 20;
-}
+enum class Month : unsigned char {
+    Jan = 1, January = Jan,
+    Feb, February = Feb,
+    Mar, March = Mar,
+    Apr, April = Apr,
+    May,
+    Jun, June = Jun,
+    Jul, July = Jul,
+    Aug, August = Aug,
+    Sep, September = Sep,
+    Oct, October = Oct,
+    Nov, November = Nov,
+    Dec, December = Dec
+};
 
 int main() {
-    std::cout << "Hello, World in C++" << get_version() << "!" << std::endl;
-    return 0;
+    Month month {Month::Feb};
+    Month month2 {Month::February};
+    
+    std::cout << "month: " << static_cast<unsigned int>(month) << std::endl;
+    std::cout << "sizeof(month): " << sizeof(month) << std::endl;
+    
+    std::cout << "month2: " << static_cast<unsigned int>(month2) << std::endl;
+    std::cout << "sizeof(month2): " << sizeof(month2) << std::endl;
+    
+    std::cout << std::boolalpha;
+    
+    std::cout << "(month == month2): " << (month == month2) << std::endl;
 }
