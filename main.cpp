e3a76ac94abd653aaf509307ca5ac3920dfdb599
Raphael Manayon <133437120+rephy@users.noreply.github.com>
2024-11-07 07:55:59 -0800

    Video 3

--- main.cpp
+++ main.cpp
@@ -16,14 +16,33 @@ enum class Month : unsigned char {
     Dec, December = Dec
 };
 
+std::string_view month_to_string(Month month) {
+    using enum Month;
+    
+    switch (month) {
+        case Jan: return "January";
+        case Feb: return "February";
+        case Mar: return "March";
+        case Apr: return "April";
+        case May: return "May";
+        case Jun: return "June";
+        case Jul: return "July";
+        case Aug: return "August";
+        case Sep: return "September";
+        case Oct: return "October";
+        case Nov: return "November";
+        case Dec: return "December";
+    }
+}
+
 int main() {
     Month month {Month::Feb};
     Month month2 {Month::February};
     
-    std::cout << "month: " << static_cast<unsigned int>(month) << std::endl;
+    std::cout << "month: " << month_to_string(month) << std::endl;
     std::cout << "sizeof(month): " << sizeof(month) << std::endl;
     
-    std::cout << "month2: " << static_cast<unsigned int>(month2) << std::endl;
+    std::cout << "month2: " << month_to_string(month2) << std::endl;
     std::cout << "sizeof(month2): " << sizeof(month2) << std::endl;
     
     std::cout << std::boolalpha;
